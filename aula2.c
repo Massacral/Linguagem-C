9719b612e75ee26d64236e35b89c77e4edc529fe
Tiago Martins De Oliveira <tiago.859@gmail.com>
2025-06-05 17:03:29 -0300

    Add files via upload

---
+++ aula2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main (){
+    int idade = 25;
+    float altura = 1.75;
+    char letra = 'A';
+    
+    printf("idade: %d\n", idade);
+    printf("Altura: %d\n", altura);
+    printf("Letra: %d\n", letra);
+
+    return 0;
+} 
